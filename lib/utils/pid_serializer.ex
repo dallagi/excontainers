0ada524d9caaeaece90858438375ee7c514ade35
Marco Dallagiacoma <marco.dallagiacoma@gmail.com>
2020-12-24 23:42:07 +0100

    Add pid serializer

---
+++ lib/utils/pid_serializer.ex
@@ -0,0 +1,13 @@
+defmodule Excontainers.PidSerializer do
+  @spec serialize(pid()) :: binary
+  def serialize(pid) do
+    pid
+    |> inspect
+    |> remove_chars("#<>")
+    |> replace_dots_with_dashes
+  end
+
+  defp remove_chars(string, chars), do: String.replace(string, ~r/[#{chars}]/, "")
+
+  defp replace_dots_with_dashes(string), do: String.replace(string, ~r/\./, "-")
+end
