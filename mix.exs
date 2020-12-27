2c30e5c8fa59b39224a6d18eab843ac99ce81f45
Marco Dallagiacoma <marco.dallagiacoma@gmail.com>
2020-12-27 13:46:15 +0100

    Fix warnings about :crypto

--- mix.exs
+++ mix.exs
@@ -15,7 +15,7 @@ defmodule Excontainers.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: [:logger, :crypto]
     ]
   end
 
