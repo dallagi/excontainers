d444980565d3ec71c912318231b4ede2615d8c89
Marco Dallagiacoma <marco.dallagiacoma@gmail.com>
2020-12-27 22:09:46 +0100

    Group tests using describe

--- test/docker_test.exs
+++ test/docker_test.exs
@@ -7,82 +7,92 @@ defmodule DockerTest do
     :ok
   end
 
-  test "ping/0 returns :ok when communication with docker is successful" do
-    assert Docker.ping() == :ok
+  describe "ping/0" do
+    test "returns :ok when communication with docker is successful" do
+      assert Docker.ping() == :ok
+    end
+
+    test "returns error when communication with docker fails" do
+      mock_docker_host("tcp://invalid-docker-host:1234")
+
+      assert {:error, _} = Docker.ping()
+    end
   end
 
-  test "ping/0 returns error when communication with docker fails" do
-    mock_docker_host("tcp://invalid-docker-host:1234")
+  describe "inspect_container/1" do
+    test "returns info about running container" do
+      with_running_container(fn container_id ->
+        expected_container_info = %Docker.Container{
+          id: container_id,
+          status: %Docker.Container.Status{state: :running, running: true}
+        }
 
-    assert {:error, _} = Docker.ping()
+        assert {:ok, ^expected_container_info} = Docker.inspect_container(container_id)
+      end)
+    end
+
+    test "returns error when container does not exist" do
+      assert {:error, _} = Docker.inspect_container("unexisting-container-#{UUID.uuid4()}")
+    end
   end
 
-  test "inspect_container/1 returns info about running container" do
-    with_running_container(fn container_id ->
-      expected_container_info = %Docker.Container{
-        id: container_id,
-        status: %Docker.Container.Status{state: :running, running: true}
-      }
+  describe "create_container/2" do
+    test "creates a container with the specified config" do
+      unique_container_name = "test_create_container_#{UUID.uuid4()}"
+      config = %Docker.ContainerConfig{image: "alpine:20201218", cmd: ["sleep", "infinity"]}
+      on_exit(fn -> remove_container(unique_container_name) end)
 
-      assert {:ok, ^expected_container_info} = Docker.inspect_container(container_id)
-    end)
+      {:ok, container_id} = Docker.create_container(config, unique_container_name)
+
+      {all_containers_output, _exit_code = 0} = System.cmd("docker", ["ps", "-a"])
+      assert all_containers_output =~ unique_container_name
+      assert all_containers_output =~ String.slice(container_id, 1..11)
+    end
+
+    test "returns error when container configuration is invalid" do
+      config = %Docker.ContainerConfig{image: "invalid image"}
+
+      assert {:error, _} = Docker.create_container(config)
+    end
   end
 
-  test "inspect_container/1 returns error when container does not exist" do
-    assert {:error, _} = Docker.inspect_container("unexisting-container-#{UUID.uuid4()}")
+  describe "start_container/1" do
+    test "starts a created container" do
+      with_created_container(fn container_id ->
+        :ok = Docker.start_container(container_id)
+
+        {running_containers_output, _exit_code = 0} = System.cmd("docker", ["ps"])
+        assert running_containers_output =~ String.slice(container_id, 1..11)
+      end)
+    end
+
+    test "returns error when container does not exist" do
+      assert {:error, _} = Docker.start_container("unexisting-container-#{UUID.uuid4()}")
+    end
   end
 
-  test "create_container/2 creates a container with the specified config" do
-    unique_container_name = "test_create_container_#{UUID.uuid4()}"
-    config = %Docker.ContainerConfig{image: "alpine:20201218", cmd: ["sleep", "infinity"]}
-    on_exit(fn -> remove_container(unique_container_name) end)
+  describe "stop_container/1" do
+    test "stops a running container" do
+      with_running_container(fn container_id ->
+        :ok = Docker.stop_container(container_id, timeout_seconds: 1)
 
-    {:ok, container_id} = Docker.create_container(config, unique_container_name)
+        {running_containers_output, _exit_code = 0} = System.cmd("docker", ["ps"])
+        assert not (running_containers_output =~ String.slice(container_id, 1..11))
+      end)
+    end
 
-    {all_containers_output, _exit_code = 0} = System.cmd("docker", ["ps", "-a"])
-    assert all_containers_output =~ unique_container_name
-    assert all_containers_output =~ String.slice(container_id, 1..11)
-  end
+    test "returns :ok and does nothing if container was already stopped" do
+      with_created_container(fn container_id ->
+        :ok = Docker.stop_container(container_id)
 
-  test "create_container/2 returns error when container configuration is invalid" do
-    config = %Docker.ContainerConfig{image: "invalid image"}
+        {running_containers_output, _exit_code = 0} = System.cmd("docker", ["ps"])
+        assert not (running_containers_output =~ String.slice(container_id, 1..11))
+      end)
+    end
 
-    assert {:error, _} = Docker.create_container(config)
-  end
-
-  test "start_container/1 starts a created container" do
-    with_created_container(fn container_id ->
-      :ok = Docker.start_container(container_id)
-
-      {running_containers_output, _exit_code = 0} = System.cmd("docker", ["ps"])
-      assert running_containers_output =~ String.slice(container_id, 1..11)
-    end)
-  end
-
-  test "start_container/1 returns error when container does not exist" do
-    assert {:error, _} = Docker.start_container("unexisting-container-#{UUID.uuid4()}")
-  end
-
-  test "stop_container/1 stops a running container" do
-    with_running_container(fn container_id ->
-      :ok = Docker.stop_container(container_id, timeout_seconds: 1)
-
-      {running_containers_output, _exit_code = 0} = System.cmd("docker", ["ps"])
-      assert not (running_containers_output =~ String.slice(container_id, 1..11))
-    end)
-  end
-
-  test "stop_container/1 returns :ok and does nothing if container was already stopped" do
-    with_created_container(fn container_id ->
-      :ok = Docker.stop_container(container_id)
-
-      {running_containers_output, _exit_code = 0} = System.cmd("docker", ["ps"])
-      assert not (running_containers_output =~ String.slice(container_id, 1..11))
-    end)
-  end
-
-  test "stop_container/1 returns error when container does not exist" do
-    assert {:error, _} = Docker.stop_container("unexisting-container-#{UUID.uuid4()}")
+    test "returns error when container does not exist" do
+      assert {:error, _} = Docker.stop_container("unexisting-container-#{UUID.uuid4()}")
+    end
   end
 
   defp mock_docker_host(mocked_value) do
