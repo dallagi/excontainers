e7d6dedd00cd8afe92d76e9893d0da2004461d65
Marco Dallagiacoma <marco.dallagiacoma@gmail.com>
2020-12-28 19:33:17 +0100

    Remove unused environment-related modules

--- lib/environment.ex
+++
@@ -1,6 +0,0 @@
-defmodule Environment do
-  @doc """
-  Returns the value of an environment variable, or a default if it is not set.
-  """
-  @callback get(String.t(), String.t()) :: String.t()
-end
--- lib/system_environment.ex
+++
@@ -1,6 +0,0 @@
-defmodule SystemEnvironment do
-  @behaviour Environment
-
-  @impl Environment
-  def get(key, default \\ nil), do: System.get_env(key, default)
-end
