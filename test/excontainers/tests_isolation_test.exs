91a9f63dce2f8142bb5bf7dfd6283de7c80dad8d
Marco Dallagiacoma <marco.dallagiacoma@gmail.com>
2020-12-29 22:56:48 +0100

    Add (ugly) test about isolation of tests
    i.e., testing that each test will have a newly created container

---
+++ test/excontainers/tests_isolation_test.exs
@@ -0,0 +1,51 @@
+defmodule Excontainers.TestsIsolationTest do
+  use ExUnit.Case
+
+  import ExUnit.CaptureIO
+
+  test "containers are re-created for each test" do
+
+    defmodule SampleTest do
+      use ExUnit.Case
+      use Excontainers.ExUnit
+      alias Excontainers.Container
+
+      container(:alpine, Container.new("alpine:20201218", cmd: ["sleep", "infinity"]))
+
+      test "a test" do
+        IO.puts("<container_id:#{Container.info(:alpine).id}>")
+      end
+
+      test "another test" do
+        IO.puts("<container_id:#{Container.info(:alpine).id}>")
+      end
+    end
+
+    load_ex_unit()
+    run_tests = fn -> assert ExUnit.run() == %{failures: 0, skipped: 0, total: 2, excluded: 0} end
+
+    [first_container_id, second_container_id] =
+      capture_io(run_tests)
+      |> parse_containers_ids_from_tests_output()
+
+    assert first_container_id != second_container_id
+  end
+
+  defp load_ex_unit do
+    ExUnit.Server.modules_loaded()
+    configure_and_reload_on_exit()
+  end
+
+  defp parse_containers_ids_from_tests_output(tests_output) do
+    Regex.scan(~r{<container_id:([0-9a-f]+)>}, tests_output)
+    |> Enum.map(fn [_matched_substring, container_id] -> container_id end)
+  end
+
+  defp configure_and_reload_on_exit() do
+    old_opts = ExUnit.configuration()
+
+    ExUnit.configure([autorun: false, seed: 0, colors: [enabled: false], exclude: [:exclude]])
+
+    on_exit(fn -> ExUnit.configure(old_opts) end)
+  end
+end
