b8594eb17e49106e6d5ffdbc20c69e6bac7a2e82
Marco Dallagiacoma <marco.dallagiacoma@gmail.com>
2020-12-31 14:06:08 +0100

    Wait for exec to finish before returning its result

--- lib/docker/exec.ex
+++ lib/docker/exec.ex
@@ -6,11 +6,25 @@ defmodule Docker.Exec do
   end
 
   def exec_and_wait(container_id, command) do
+    {:ok, exec_id} = exec(container_id, command)
+    {:ok, exec_info} = wait_for_exec_result(exec_id)
+
+    {:ok, {exec_info.exit_code, ""}}
+  end
+
+  def exec(container_id, command) do
     {:ok, exec_id} = create_exec(container_id, command)
     :ok = start_exec(exec_id)
-    {:ok, exec} = inspect_exec(exec_id)
 
-    {:ok, {exec.exit_code, ""}}
+    {:ok, exec_id}
+  end
+
+  def inspect_exec(exec_id) do
+    case Client.get("/exec/#{exec_id}/json") do
+      {:ok, %{status: 200, body: body}} -> {:ok, parse_inspect_result(body)}
+      {:ok, %{status: status}} -> {:error, {:http_error, status}}
+      {:error, message} -> {:error, message}
+    end
   end
 
   defp create_exec(container_id, command) do
@@ -31,15 +45,16 @@ defmodule Docker.Exec do
     end
   end
 
-  defp inspect_exec(exec_id) do
-    case Client.get("/exec/#{exec_id}/json") do
-      {:ok, %{status: 200, body: body}} -> {:ok, parse_inspect_result(body)}
-      {:ok, %{status: status}} -> {:error, {:http_error, status}}
-      {:error, message} -> {:error, message}
-    end
-  end
-
   defp parse_inspect_result(json) do
     %ExecStatus{running: json["Running"], exit_code: json["ExitCode"]}
+  end
+
+  defp wait_for_exec_result(exec_id) do
+    case Docker.Exec.inspect_exec(exec_id) do
+      {:ok, %ExecStatus{running: true}} ->
+        :timer.sleep(100)
+        wait_for_exec_result(exec_id)
+      {:ok, finished_exec_status} -> {:ok, finished_exec_status}
+    end
   end
 end
--- test/docker/exec_test.exs
+++ test/docker/exec_test.exs
@@ -1,10 +1,21 @@
 defmodule Docker.ExecTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: true
+
+  describe "exec/2" do
+    test "starts command inside container and instantly returns exec id" do
+      with_running_container(fn container_id ->
+        assert {:ok, exec_id} = Docker.Exec.exec(container_id, ["sleep", "1"])
+
+        {:ok, exec_status} = Docker.Exec.inspect_exec(exec_id)
+        assert exec_status.running
+      end)
+    end
+  end
 
   describe "exec_and_wait/2" do
     test "runs command inside container and returns its exit code once it finishes" do
       with_running_container(fn container_id ->
-        assert {:ok, {0, ""}} = Docker.exec_and_wait(container_id, ["echo", "hi", "there"])
+        assert {:ok, {0, ""}} = Docker.Exec.exec_and_wait(container_id, ["sleep", "1"])
       end)
     end
   end
