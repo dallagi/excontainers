843a791b0265bb28b4f17b3fe8c3c1be556f23a9
Marco Dallagiacoma <marco.dallagiacoma@gmail.com>
2021-01-01 19:08:49 +0100

    Use /_ping endpoint for ping, instead of /info

--- lib/docker.ex
+++ lib/docker.ex
@@ -2,7 +2,7 @@ defmodule Docker do
   alias Docker.{Client, Container}
 
   def ping() do
-    case Client.get("/info") do
+    case Client.get("/_ping") do
       {:ok, %{status: 200}} -> :ok
       {:ok, %{status: status}} -> {:error, {:http_error, status}}
       {:error, message} -> {:error, message}
