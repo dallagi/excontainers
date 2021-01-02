64cf518556f8249835493d36a5b7f42e74418b31
Marco Dallagiacoma <marco.dallagiacoma@gmail.com>
2021-01-02 22:15:56 +0100

    Add customization options for postgres container

--- lib/excontainers/postgres_container.ex
+++ lib/excontainers/postgres_container.ex
@@ -1,16 +1,34 @@
 defmodule Excontainers.PostgresContainer do
-  alias Excontainers.{Containers, CommandWaitStrategy}
+  alias Excontainers.{Container, Containers, CommandWaitStrategy}
 
-  @exposed_ports [5432]
-  @environment %{POSTGRES_USER: "test", POSTGRES_PASSWORD: "test", POSTGRES_DB: "test"}
+  @postgres_port 5432
+  # @environment %{POSTGRES_USER: "test", POSTGRES_PASSWORD: "test", POSTGRES_DB: "test"}
   @wait_strategy CommandWaitStrategy.new(["pg_isready", "-U", "test", "-d", "test", "-h", "localhost"])
 
   def new(image \\ "postgres:13.1", opts \\ []) do
     Containers.new(
       image,
-      exposed_ports: @exposed_ports,
-      environment: @environment,
+      exposed_ports: [@postgres_port],
+      environment: %{
+        POSTGRES_USER: Keyword.get(opts, :username, "test"),
+        POSTGRES_PASSWORD: Keyword.get(opts, :password, "test"),
+        POSTGRES_DB: Keyword.get(opts, :database, "test")
+      },
       wait_strategy: @wait_strategy
     )
+  end
+
+  def port(pid), do: Container.mapped_port(pid, @postgres_port)
+
+  def connection_parameters(pid) do
+    config = Container.config(pid)
+
+    [
+      hostname: "localhost",
+      port: port(pid),
+      username: config.environment[:POSTGRES_USER],
+      password: config.environment[:POSTGRES_PASSWORD],
+      database: config.environment[:POSTGRES_DB]
+    ]
   end
 end
--- test/excontainers/postgres_container_test.exs
+++ test/excontainers/postgres_container_test.exs
@@ -2,22 +2,41 @@ defmodule Excontainers.PostgresContainerTest do
   use ExUnit.Case, async: true
   use Excontainers.ExUnit
 
-  alias Excontainers.{Container, PostgresContainer}
+  alias Excontainers.PostgresContainer
 
   describe "with default configuration" do
     container(:postgres, PostgresContainer.new())
 
     test "provides a ready-to-use postgres container" do
-      {:ok, pid} =
-        Postgrex.start_link(
-          hostname: "localhost",
-          port: Container.mapped_port(:postgres, 5432),
-          username: "test",
-          password: "test",
-          database: "test"
-        )
+      {:ok, pid} = Postgrex.start_link(PostgresContainer.connection_parameters(:postgres))
 
       assert %{num_rows: 1} = Postgrex.query!(pid, "SELECT 1", [])
+    end
+  end
+
+  describe "with custom configuration" do
+    @custom_postgres PostgresContainer.new("postgres:12.1",
+                       username: "custom-user",
+                       password: "custom-password",
+                       database: "custom-database"
+                     )
+
+    container(:postgres, @custom_postgres)
+
+    test "provides a postgres container compliant with specified configuration" do
+      {:ok, pid} =
+        Postgrex.start_link(
+          username: "custom-user",
+          password: "custom-password",
+          database: "custom-database",
+          hostname: "localhost",
+          port: PostgresContainer.port(:postgres)
+        )
+
+      query_result = Postgrex.query!(pid, "SELECT version()", [])
+
+      version_info = query_result.rows |> Enum.at(0) |> Enum.at(0)
+      assert version_info =~ "PostgreSQL 12.1"
     end
   end
 end
