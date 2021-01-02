ed7f885c34c5af6a5ac22e3ff6d8082d602532e4
Marco Dallagiacoma <marco.dallagiacoma@gmail.com>
2021-01-02 14:07:05 +0100

    Make elixir_uuid a test-only dependency

--- mix.exs
+++ mix.exs
@@ -31,12 +31,12 @@ defmodule Excontainers.MixProject do
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
 
-      {:elixir_uuid, "~> 1.2"},
       {:hackney, "~> 1.16.0"},
       {:jason, ">= 1.0.0"},
       {:tesla, "~> 1.4.0"},
       {:gestalt, "~> 1.0"},
       {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
+      {:elixir_uuid, "~> 1.2", only: [:dev, :test]},
       {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.0", only: [:dev], runtime: false},
       {:postgrex, "~> 0.15", only: [:dev, :test]},
