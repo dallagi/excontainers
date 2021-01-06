b7cc7799175cbefccac5583cc2207d639483a635
Marco Dallagiacoma <marco.dallagiacoma@gmail.com>
2021-01-06 14:43:53 +0100

    Add builder-style api to configure docker container

--- lib/docker/container_config.ex
+++ lib/docker/container_config.ex
@@ -1,4 +1,6 @@
 defmodule Docker.ContainerConfig do
+  alias Docker.VolumeBinding
+
   @enforce_keys [:image]
   defstruct [
     :image,
@@ -9,4 +11,17 @@ defmodule Docker.ContainerConfig do
     privileged: false,
     bind_mounts: []
   ]
-end
+
+  def with_environment(config, key, value) do
+    %__MODULE__{config | environment: Map.put(config.environment, key, value)}
+  end
+
+  def with_exposed_port(config, port) do
+    %__MODULE__{config | exposed_ports: [port | config.exposed_ports]}
+  end
+
+  def with_bind_mount(config, host_src, container_dest, options \\ "ro") do
+    new_bind_mount = %VolumeBinding{host_src: host_src, container_dest: container_dest, options: options}
+    %__MODULE__{config | bind_mounts: [ new_bind_mount | config.bind_mounts ]}
+  end
+ end
---
+++ test/docker/container_config_test.exs
@@ -0,0 +1,37 @@
+defmodule Docker.ContainerConfigTest do
+  use ExUnit.Case, async: true
+
+  alias Docker.ContainerConfig
+
+  test "can be modified with a builder-like approach" do
+    config =
+      %ContainerConfig{image: "my-image"}
+      |> ContainerConfig.with_environment("key1", "val1")
+      |> ContainerConfig.with_environment("key2", "val2")
+      |> ContainerConfig.with_bind_mount("/host/src", "/container/dest")
+      |> ContainerConfig.with_bind_mount("/another/host/src", "/another/container/dest")
+      |> ContainerConfig.with_exposed_port(8080)
+      |> ContainerConfig.with_exposed_port(8081)
+
+    assert config == %Docker.ContainerConfig{
+             bind_mounts: [
+               %Docker.VolumeBinding{
+                 container_dest: "/another/container/dest",
+                 host_src: "/another/host/src",
+                 options: "ro"
+               },
+               %Docker.VolumeBinding{
+                 container_dest: "/container/dest",
+                 host_src: "/host/src",
+                 options: "ro"
+               }
+             ],
+             cmd: nil,
+             environment: %{"key1" => "val1", "key2" => "val2"},
+             exposed_ports: [8081, 8080],
+             image: "my-image",
+             privileged: false,
+             wait_strategy: nil
+           }
+  end
+end
